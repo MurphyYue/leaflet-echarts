18b966ec5e37ce6fc538eb66ea28bc862a6fd670
murphy <1009098371@qq.com>
2018-01-15 16:05:24 +0800

    添加标注点、echarts

--- src/components/helloMap.vue
+++ src/components/helloMap.vue
@@ -4,31 +4,56 @@
 <script>
 import leaflet from "leaflet";
 import mapProvider from "../common/js/mapProviders.js";
+import * as echarts from 'echarts';
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
-//const MAP_IMAGE_PATH = "../../node_modules/leaflet/dist/images/";
+/* const MAP_IMAGE_PATH = "../common/images/leaflet/"; */
 export default {
   data() {
     return {
-                map: null,
-                markers: null,
-                geoJsonLayer: null,
-                init_map_data: null,
-                reset_btn: null,
-                map_config: {
-                    zoom: 15,
-                    center: [39.92, 116.39],
-                    minZoom: 3,
-                    maxZoom: 18,
-                    attribution: "&copy; 高德地图",
-                },
-            };
+      map: null,
+      markers: null,
+      circle: null,
+      polygon: null,
+      mapChart: null,
+      geoJsonLayer: null,
+      init_map_data: null,
+      reset_btn: null,
+      map_config: {
+        zoom: 15,
+        center: [39.92, 116.392],
+        minZoom: 3,
+        maxZoom: 18,
+        attribution: "&copy; 高德地图"
+      },
+      option: {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        }
+    };
   },
   mounted() {
     this.initMap();
+    this.addSymble()
   },
   methods: {
     /* initMap() {
-      //L.Icon.Default.imagePath = MAP_IMAGE_PATH;
+      L.Icon.Default.imagePath = MAP_IMAGE_PATH;
 
       this.mymap = L.map("map").setView([39.9, 116.36], 13)
       L.tileLayer(
@@ -43,11 +68,18 @@ export default {
       ).addTo(this.mymap);
     } */
     initMap() {
+      /*  L.Icon.Default.imagePath = MAP_IMAGE_PATH; */
+      let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow
+      });
+      L.Marker.prototype.options.icon = DefaultIcon;
       this.map = L.map("map", {
         center: this.map_config.center,
         zoom: this.map_config.zoom,
         minZoom: this.map_config.minZoom,
-        maxZoom: this.map_config.maxZoom
+        maxZoom: this.map_config.maxZoom,
+        zoomControl: false
         // scrollWheelZoom: false,
       });
       L.tileLayer
@@ -55,7 +87,33 @@ export default {
           attribution: this.map_config.attribution
         })
         .addTo(this.map);
+      
+      L.control.zoom({
+        zoomInTitle: '放大',
+        zoomOutTitle: '缩小'
+      }).addTo(this.map)
+    },
+    addSymble() {
+      this.markers = L.marker(this.map_config.center).addTo(this.map);
+      this.circle = L.circle([39.92, 116.392],{
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+      }).addTo(this.map)
+      this.polygon = L.polygon([
+        [39.91, 116.392],
+        [39.92, 116.397],
+        [39.92, 116.392]
+      ]).addTo(this.map)
+      this.markers.bindPopup("<div id='mapChart'></div>")
+      this.markers.on('click',this.drawChart)
+    },
+    drawChart() {
+      this.mapChart = echarts.init(document.getElementById('mapChart'))
+      this.mapChart.setOption(this.option)
     }
+
   }
 };
 </script>
@@ -64,6 +122,10 @@ export default {
 
 #map {
   height: 100vh;
+}
+#mapChart{
+  width: 200px;
+  height: 150px;
 }
 </style>
 
